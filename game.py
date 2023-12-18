d7bca48d642fa25ad4a0066ee4044985a0178672
Alejandro Giner <alejandroginerpaez@gmail.com>
2023-12-18 11:09:45 +0100

    default board

--- game.py
+++ game.py
@@ -1,8 +1,24 @@
 class BattleshipGame():
     def __init__(self, max_turns: int, size: int):
         self.max_turns = max_turns
-        self.board = [['.' for i in range(size)] for i in range(size)]
+        self.current_turn = 0
+        self.game_over = False
+        #self.board = [['.' for i in range(size)] for i in range(size)]
         self.discovered = [[False for i in range(size)] for i in range(size)]
+
+        default_board = []
+        default_board.append('..........')
+        default_board.append('.L......P.')
+        default_board.append('........P.')
+        default_board.append('........P.')
+        default_board.append('...CC...P.')
+        default_board.append('........P.')
+        default_board.append('...S......')
+        default_board.append('...S......')
+        default_board.append('...S......')
+        default_board.append('..BBBB....')
+        self.board = default_board
+
     
     def get_board(self):
         board = []
@@ -13,3 +29,7 @@ class BattleshipGame():
             board_str += ''.join(row)
             board_str += '\n'
         return board_str
+
+    def comprueba(self, row, col):
+        row = 'ABCDEFGHIJ'.find(row)
+        col -= 1
