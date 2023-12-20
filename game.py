7cb592c08609f9fceeab824f3514b6ff6d597176
Alejandro Giner <alejandroginerpaez@gmail.com>
2023-12-20 13:48:50 +0100

    stuff

--- game.py
+++ game.py
@@ -1,35 +1,66 @@
 class BattleshipGame():
-    def __init__(self, max_turns: int, size: int):
+    def __init__(self, max_turns: int, size: int = 10):
         self.max_turns = max_turns
+        self.size = size
         self.current_turn = 0
         self.game_over = False
         #self.board = [['.' for i in range(size)] for i in range(size)]
         self.discovered = [[False for i in range(size)] for i in range(size)]
 
+        # Representa las secciones que quedan en pie de cada barco
+        self.ships = {
+            'L':1,
+            'C':2,
+            'S':3,
+            'B':4,
+            'P':5
+        }
+        self.rows = 'ABCDEFGHIJ'
+
         default_board = []
-        default_board.append('..........')
-        default_board.append('.L......P.')
-        default_board.append('........P.')
-        default_board.append('........P.')
-        default_board.append('...CC...P.')
-        default_board.append('........P.')
-        default_board.append('...S......')
-        default_board.append('...S......')
-        default_board.append('...S......')
-        default_board.append('..BBBB....')
+        default_board.append('~~~~~~~~~~')
+        default_board.append('~L~~~~~~P~')
+        default_board.append('~~~~~~~~P~')
+        default_board.append('~~~~~~~~P~')
+        default_board.append('~~~CC~~~P~')
+        default_board.append('~~~~~~~~P~')
+        default_board.append('~~~S~~~~~~')
+        default_board.append('~~~S~~~~~~')
+        default_board.append('~~~S~~~~~~')
+        default_board.append('~~BBBB~~~~')
         self.board = default_board
 
     
     def get_board(self):
         board = []
+        esc = '\x1b'
+        colors = {
+            '~': '[44m',
+            '?': '[45m',
+            'L': '[41m',
+            'C': '[41m',
+            'S': '[41m',
+            'B': '[41m',
+            'P': '[41m'
+        }
+ 
+        # Números de las columnas
+        board.append([str(i+1) for i in range(self.size)])
+
+        # Reemplazamos por ? las casillas que no hayan sido exploradas
         for i, row in enumerate(self.board):
             board.append([(cell if self.discovered[i][j] else '?') for j, cell in enumerate(row)])
+
         board_str = ''
-        for row in board:
-            board_str += ''.join(row)
-            board_str += '\n'
+        for n, row in enumerate(board):
+            #board_str += ' '.join(row)
+            for i in row:
+                if i in colors.keys():
+                    board_str += f'{esc}{colors.get(i)}'
+                board_str += f'{i: ^4}{esc}[0m'
+            board_str += f'{self.rows[n-1] if n>0 else "": ^3}\n'
         return board_str
 
     def comprueba(self, row, col):
-        row = 'ABCDEFGHIJ'.find(row)
+        row = self.rows.find(row)
         col -= 1
--- main.py
+++ main.py
@@ -1,4 +1,96 @@
 import game
+import pickle
+import os
+import re
 
-current_game = game.BattleshipGame(35,10)
-print(current_game.get_board())
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
+
+
+def get_option_from_list(options: dict):
+    while True:
+        for i,v in options.items():
+            print(f'{i}. {v[0]}')
+        eleccion = input()
+        if eleccion in options.keys():
+            return eleccion
+        print('Opción inválida. Por favor elige una opción de la lista.')
+
+def guardar():
+    pass
+
+def turno(jugada):
+    if jugada == 'G':
+        guardar()
+        return 'Partida guardada.'
+    elif jugada_regex := re.fullmatch(r'([A-J])([0-9]+)',jugada):
+        row, col = jugada_regex.group(1,2)
+        return f'Elegida casilla {row}{col}'
+
+def ejecutar_partida(current_game: game.BattleshipGame):
+    resultado_turno = ''
+    while True:
+        cls()
+        print(current_game.get_board())
+        print(f'Turno {current_game.current_turn} de {current_game.max_turns}')
+        print(resultado_turno)
+        print('Introduce una coordenada para intentar destruir un barco (por ejemplo: B3)')
+        print('O introduce \'G\' para guardar la partida.')
+        jugada = input()
+        resultado_turno = turno(jugada)
+
+
+
+def nueva_partida():
+    difficulties = {
+        '1': ('Fácil', 60),
+        '2': ('Difícil', 35)
+    }
+    print('Elige una dificultad:')
+    choice = get_option_from_list(difficulties)
+    chosen_difficulty = difficulties[choice][1]
+    new_game = game.BattleshipGame(chosen_difficulty)
+    ejecutar_partida(new_game)
+
+
+def cargar_partida():
+    try:
+        with open('battleship.pickle','rb') as f:
+            loaded_game = pickle.load(f)
+    except:
+        print('No se ha podido cargar la partida.')
+        return
+    ejecutar_partida(loaded_game)
+
+opciones = {
+    '1':('Nueva partida',nueva_partida),
+    '0':('Salir',exit)
+}
+
+def hay_partida_anterior():
+    try:
+        with open('battleship.pickle','rb') as f:
+            return pickle.load(f)
+    except:
+        return False
+
+def menu():
+    while True:
+        if hay_partida_anterior():
+            opciones['2'] = ('Cargar partida',cargar_partida)
+        elif opciones.get('2'):
+            opciones.pop('2')
+        for i,v in opciones.items():
+            print(f'{i}. {v[0]}')
+        eleccion = input()
+        if eleccion in opciones.keys():
+            return eleccion
+        print('Opción inválida. Por favor elige una opción de la lista.')
+
+def main():
+    while True:
+        eleccion = menu()
+        opciones[eleccion][1]()
+
+if __name__ == '__main__':
+    main()
